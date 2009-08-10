1d33ed48bbb77463617eca21841a4d90083345ed
Dima Kogan <dkogan@cds.caltech.edu>
2009-08-10 12:57:49 -0700

    number of streams now dynamically parsed
    Ignore-this: 6c27c400e38c516d9cddc6d19630916e
    
    darcs-hash:20090810195749-0cb85-3b7edfccd8d0175b4f2eba7e24187d3ea9678115.gz

--- driveGnuPlots.pl
+++ driveGnuPlots.pl
@@ -4,6 +4,12 @@ use Getopt::Long;
 use Time::HiRes qw( usleep );
 use Data::Dumper;
 
+# list containing the plot data. Each element is a hash describing the extra
+# plotting options for each curve we're plotting, and the actual data to
+# plot for each curve. The length of this list grows as the data comes
+# in
+my @curves = ();
+
 # stream in the data by default
 # point plotting by default
 my %options = ( "stream" => 1,
@@ -18,12 +24,14 @@ GetOptions(\%options,
            "ylabel=s",
            "y2label=s",
            "title=s",
+           "xlen=i",
            "ymin=f",
            "ymax=f",
            "y2min=f",
            "y2max=f",
            "y2=i@",
-           "hardcopy=s");
+           "hardcopy=s",
+           "help");
 
 # set up plotting style
 my $style = "";
@@ -35,12 +43,6 @@ if(!$style) { $style = "points"; }
 sub usage {
     print "Usage: $0 <options>\n";
     print <<OEF;
-where mandatory options are (in order):
-
-  NumberOfStreams                       How many streams to plot
-  Stream_WindowSampleSize               this many samples
-
-also
   --[no]stream         Do [not] display the data a point at a time, as it comes in
   --[no]lines          Do [not] draw lines to connect consecutive points
   --xlabel xxx         Set x-axis label
@@ -48,6 +50,7 @@ also
   --y2label xxx        Set y2-axis label
   --title  xxx         Set the title of the plot
   --legend xxx         Set the label for a curve plot. Give this option multiple times for multiple curves
+  --xlen xxx           Set the size of the x-window to plot
   --ymin  xxx          Set the range for the y axis. Both or neither of these have to be specified
   --ymax  xxx          Set the range for the y axis. Both or neither of these have to be specified
   --y2min xxx          Set the range for the y2 axis. Both or neither of these have to be specified
@@ -57,16 +60,12 @@ also
 OEF
 }
 
-sub Arg {
-    if ($#ARGV < $_[0]) {
-        print "Expected parameter missing...\n\n";
-        usage;
-        die("Error parsing args\n");
-    }
-    $ARGV[int($_[0])];
-}
-
 sub main {
+    if( defined $options{"help"} )
+    {
+      usage;
+      return;
+    }
     if(  defined $options{"ymin"} && !defined $options{"ymax"} ||
         !defined $options{"ymin"} &&  defined $options{"ymax"} )
     {
@@ -81,24 +80,20 @@ sub main {
     }
     if( defined $options{"hardcopy"} && $options{"stream"} )
     {
+      usage;
       die("If making a hardcopy, we shouldn't be streaming. Doing nothing\n");
     }
+    if( !defined $options{"xlen"} )
+    {
+      usage;
+      die("Must specify the size of the moving x-window. Doing nothing\n");
+    }
+    my $samples = $options{"xlen"};
 
-    my $argIdx = 0;
-    my $numberOfStreams = Arg($argIdx++);
-    print "Will display $numberOfStreams Streams...\n";
-
-    my $samples = Arg($argIdx++);
-    print "Will use a window of $samples samples\n";
-
-    my @buffers;
-    shift @ARGV; # number of streams
-    shift @ARGV; # sample size
     local *PIPE;
-
     open PIPE, "|gnuplot" || die "Can't initialize gnuplot\n";
-
     select((select(PIPE), $| = 1)[0]);
+
     my $temphardcopyfile;
     my $outputfile;
     my $outputfileType;
@@ -136,21 +131,26 @@ sub main {
     print(PIPE "set y2label \"" . $options{"y2label"} . "\"\n") if $options{"y2label"};
     print(PIPE "set title   \"" . $options{"title"  } . "\"\n") if $options{"title"};
 
-# For the specified values, set the legend entries to 'title "blah
-# blah"'. Otherwise, "notitle".
-    my @extraopts;
-    @extraopts = map({"title \"$_\""} @{$options{"legend"}}) if($options{"legend"});
-    push @extraopts, ("notitle") x ($numberOfStreams - @extraopts);
-
-# For the values requested to be printed on the y2 axis, set that
-    foreach my $y2idx (@{$options{"y2"}}) { $extraopts[$y2idx] .= " axes x1y2 linewidth 3"; }
-
-# This is ugly, but "([]) x $numberOfStreams" was giving me references into a single physical list
-    for(my $i=0; $i<$numberOfStreams; $i++) {
-      push @buffers, [];
+# For the specified values, set the legend entries to 'title "blah blah"'
+    if($options{"legend"})
+    {
+      foreach (@{$options{"legend"}}) { newCurve($_, "") }
     }
 
-    my $streamIdx = 0;
+# For the values requested to be printed on the y2 axis, set that
+    foreach my $y2idx (@{$options{"y2"}})
+    {
+      my $str = " axes x1y2 linewidth 3";
+      if(exists $curves[$y2idx])
+      {
+        $curves[$y2idx]{"extraopts"} .= $str;
+      }
+      else
+      {
+        newCurve("", $str);
+      }
+    }
+
     select((select(STDOUT), $| = 1)[0]);
     my $xlast = 0;
 
@@ -158,22 +158,41 @@ sub main {
     my $numRE = qr/([-]?[0-9\.]+(?:e[-]?[0-9]+)?)/;
     while(<>)
     {
-      chomp;
-      my $line = $_;
-      foreach my $point ($line =~ /$numRE/g) {
-        my $buf = $buffers[$streamIdx];
+      foreach my $curve (@curves)
+      {
+        my $buf = $curve->{"data"};
+
+        # get the next datapoint, if there is one
+        my $point;
+        if(/($numRE)/gc)
+        {
+          $point = $1;
+        }
+        # if a point is not defined here, dup the last point we have if
+        # possible
+        elsif(@$buf)
+        {
+          $point = @$buf[$#$buf];
+        }
+        # otherwise we can do nothing with this curve, so we skip it
+        else
+        {
+          next;
+        }
 
         # data buffering (up to stream sample size)
-        push @{$buf}, $point;
-        shift @{$buf} if(@{$buf} > $samples && $options{"stream"});
-
-        $streamIdx++;
-        if ($streamIdx == $numberOfStreams) {
-          $streamIdx = 0;
-          plotStoredData($xlast, $samples, $numberOfStreams, *PIPE, \@buffers, \@extraopts) if($options{"stream"});
-          $xlast++;
-        }
+        push @$buf, $point;
+        shift @$buf if(@$buf > $samples && $options{"stream"});
       }
+
+      # if any extra data is available, create new curves for it
+      while(/($numRE)/gc)
+      {
+        newCurve("", "", $1);
+      }
+
+      plotStoredData($xlast, $samples, *PIPE) if($options{"stream"});
+      $xlast++;
     }
 
     if($options{"stream"})
@@ -183,8 +202,8 @@ sub main {
     }
     else
     {
-      $samples = @{$buffers[0]};
-      plotStoredData($xlast, $samples, $numberOfStreams, *PIPE, \@buffers, \@extraopts);
+      $samples = @{$curves[0]->{"data"}};
+      plotStoredData($xlast, $samples, *PIPE);
 
       if( defined $options{"hardcopy"})
       {
@@ -211,17 +230,20 @@ sub main {
 
 sub plotStoredData
 {
-  my ($xlast, $samples, $numberOfStreams, $pipe, $buffers, $extraopts) = @_;
+  my ($xlast, $samples, $pipe) = @_;
 
   my $x0 = $xlast - $samples + 1;
   print $pipe "set xrange [$x0:$xlast]\n";
-  print $pipe 'plot ' . join(', ' , map({ "\"-\" $_"} @$extraopts) ) . "\n";
 
-  foreach my $buf (@{$buffers})
+  my @extraopts = map {$_->{"extraopts"}} @curves;
+  print $pipe 'plot ' . join(', ' , map({ '"-"' . $_} @extraopts) ) . "\n";
+
+  foreach my $curve (@curves)
   {
+    my $buf = $curve->{"data"};
     # if the buffer isn't yet complete, skip the appropriate number of points
-    my $x = $x0 + $samples - @{$buf};
-    for my $elem (@{$buf}) {
+    my $x = $x0 + $samples - @$buf;
+    for my $elem (@$buf) {
       print $pipe "$x $elem\n";
       $x++;
     }
@@ -229,5 +251,24 @@ sub plotStoredData
   }
 }
 
+sub newCurve()
+{
+  my ($title, $opts, $newpoint) = @_;
+  if($title) { $opts = "title \"$title\" $opts" }
+  else       { $opts = "notitle $opts" }
+
+  my $data = [];
+  if (defined $newpoint)
+  {
+    my $numpoints = 1;
+    if (@curves) {
+      $numpoints = @{$curves[0]->{"data"}};
+    }
+    $data = [($newpoint) x $numpoints]
+  }
+  push ( @curves,
+         {"extraopts" => " $opts",
+          "data"      => $data} );
+}
 
 main;
